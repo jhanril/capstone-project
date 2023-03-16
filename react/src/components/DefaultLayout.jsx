35140379d5616c75ab38da9ec22070df3c7b55cb
Jhanril <jhanrilbucod@gmail.com>
2023-03-16 13:32:40 +0800

    logout function added

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,19 +1,31 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
 import Jobs from "../views/jobs";
+import axiosClient from "../axios.js";
 
 function DefaultLayout() {
-    const { currentUser, userToken } = useStateContext();
+    const { currentUser, setCurrentUser, userToken, setUserToken } =
+        useStateContext();
 
     if (!userToken) {
         return <Navigate to="login" />;
     }
 
+    const logout = (ev) => {
+        ev.preventDefault();
+        axiosClient.post("/logout").then((res) => {
+            setCurrentUser({});
+            setUserToken(null);
+        });
+    };
     console.log(currentUser);
 
     return (
         <>
             <div>userToken: {userToken}</div> <br />
+            <button onClick={(ev) => logout(ev)} className="btn">
+                Logout
+            </button>
             <div>
                 Name: {`${currentUser.firstname} ${currentUser.lastname}`}
             </div>{" "}
