e9325208f27e13868481769cb0cae17e0971d443
Jhanril <jhanrilbucod@gmail.com>
2023-03-21 09:12:06 +0800

    limit items displayed in other pages

--- react/src/views/jobs.jsx
+++ react/src/views/jobs.jsx
@@ -1,31 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from "../axios";
 import JobListItem from "../components/JobListItem";
 import { useStateContext } from "../contexts/ContextProvider";
 
 function Jobs() {
-    const { jobs } = useStateContext();
-    console.log(jobs);
+    // const { jobs } = useStateContext();
+    const [jobs, setJobs] = useState([]);
+    // console.log(jobs);
+
+    useEffect(() => {
+        axiosClient.get("job").then(({ data }) => {
+            setJobs(data.data);
+            // console.log(jobs);
+        });
+    }, []);
+    const limit = window.location.pathname !== "/jobs" ? 6 : undefined;
+
     return (
         <>
-            <div className="container mt-5 mb-3">
-                <div className="mt-5 mb-3">
-                    <div className="add-items d-flex justify-content-between">
-                        <h4>Create new job</h4>
-                        <Link
-                            to="/jobs/create"
-                            className="add btn btn-primary font-weight-bold todo-list-add-btn"
-                        >
-                            Add
-                        </Link>
-                    </div>
-                </div>
-                <hr className="mt-0 mb-4" />
-                <div className="row">
-                    {jobs.map((job) => (
-                        <JobListItem job={job} key={job.id} />
-                    ))}
-                </div>
+            <div className="row">
+                {jobs.slice(0, limit).map((job) => (
+                    <JobListItem job={job} key={job.id} />
+                ))}
             </div>
         </>
     );
